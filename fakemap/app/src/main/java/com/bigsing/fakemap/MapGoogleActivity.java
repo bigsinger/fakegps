3e3a2c0e85ac7b64ceaf4cfda253dff9e72ee1f3
hq_sunshine <1294600609@qq.com>
2018-05-28 20:10:14 +0800

    增加对谷歌地图的支持
    增加对谷歌地图的支持以及谷歌地图的选点的支持

---
+++ fakemap/app/src/main/java/com/bigsing/fakemap/MapGoogleActivity.java
@@ -0,0 +1,148 @@
+package com.bigsing.fakemap;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.bigsing.fakemap.MyMapActivity;
+import com.bigsing.fakemap.utils.PermissionUtils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by hzhuqi on 2018/5/15.
+ */
+public class MapGoogleActivity extends MyMapActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+    private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_content_google_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void initClickEvent() {
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //Toast.makeText(getApplicationContext(), "地图被点击", Toast.LENGTH_LONG).show();
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in this position"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //Toast.makeText(getApplicationContext(), "地图被长按", Toast.LENGTH_LONG).show();
+                mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in this position"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+    }
+
+    @Override
+    protected void doSearchInCity(String cityName) {
+
+    }
+
+    @Override
+    protected void doRequestLocation() {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        initClickEvent();
+        enableMyLocation();
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, false);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+            autoPositionToCurrentPosition();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+            autoPositionToCurrentPosition();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    //自动将标记移动到当前所在位置
+    private void autoPositionToCurrentPosition() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //设置Criteria服务商的信息
+        Criteria criteria = new Criteria();
+        //经度要求
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(false);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        //取得效果最好的criteria
+        String provider = locationManager.getBestProvider(criteria, true);
+        //得到坐标的相关信息
+        Location location = locationManager.getLastKnownLocation(provider);
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentPosition).title("My real position is here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+    }
+
+    private void showMissingPermissionError() {
+        //   Toast.makeText(getApplicationContext(),R.string.location_permission_denied,Toast.LENGTH_LONG).show();
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+}
